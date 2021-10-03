32c47d989e96974e477ccb1754228c1569246b0a
unknown <skhapijulhossen@gmail.com>
2021-10-03 10:51:20 +0530

    Test

---
+++ program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    int num = 0b10;
+    cout<<"Enter Your Name: "<<endl; cin>>name;
+    cout<<"Welcome "<<name<<endl;
+    cout<<num;
+    return 0;
+}
